af70426e3050e5d3cd3ccd98fe6f7df129253c71
yanfeixin123@163.com <yanfeixin123@163.com>
2017-08-08 18:34:24 +0800

    增加了刷新库和动画库

--- app/src/main/java/com/home/we/yuandiary/ui/module/main/WelcomeActivity.java
+++ app/src/main/java/com/home/we/yuandiary/ui/module/main/WelcomeActivity.java
@@ -5,30 +5,22 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
+import android.text.Editable;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupWindow;
+import android.widget.Toast;
 
 import com.home.we.yuandiary.R;
 import com.home.we.yuandiary.litehttp.AppApi;
-import com.home.we.yuandiary.litehttp.AppContext;
-import com.home.we.yuandiary.model.QuestionAndAnswer;
 import com.litesuits.http.exception.HttpException;
 import com.litesuits.http.listener.HttpListener;
-import com.litesuits.http.request.JsonRequest;
-import com.litesuits.http.request.StringRequest;
 import com.litesuits.http.response.Response;
-
-import java.io.BufferedInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 import cn.jesse.nativelogger.NLogger;
 
@@ -38,6 +30,11 @@ import cn.jesse.nativelogger.NLogger;
 
 public class WelcomeActivity extends Activity {
     private Button btn_welcome_regist;
+    private Button btn_welcome_finish_regist;
+    private EditText et_welcome_phoneNum;
+    private EditText et_welcome_pwd;
+    private EditText et_welcome_name;
+
 
     private static final String REGIST_URL = "http://114.215.238.246/api?padapi=user-userreg.php";
 
@@ -49,7 +46,7 @@ public class WelcomeActivity extends Activity {
         inits();
         regist();
 
-        getUa();
+        //getUa();
 
 
 
@@ -57,7 +54,9 @@ public class WelcomeActivity extends Activity {
     }
 
 
-
+    /**
+     * 获取user_agent
+     */
     private void getUa() {
         WebView webView = new WebView(this);
         String userAgentString = webView.getSettings().getUserAgentString();
@@ -87,13 +86,23 @@ public class WelcomeActivity extends Activity {
     private void inits() {
         btn_welcome_regist = (Button) findViewById(R.id.btn_welcome_register);
 
+
         final LayoutInflater inflater = this.getLayoutInflater();
 
-        //注册
+
+
+
+
+        //点击注册按钮，弹出注册菜单
         btn_welcome_regist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                View pop_View = inflater.inflate(R.layout.popwindow_regist, null);
+                final View pop_View = inflater.inflate(R.layout.popwindow_regist, null);
+
+                btn_welcome_finish_regist = (Button) pop_View.findViewById(R.id.btn_welcome_finish_regist);
+                et_welcome_phoneNum = (EditText) pop_View.findViewById(R.id.et_welcome_phoneNum);
+                et_welcome_pwd = (EditText) pop_View.findViewById(R.id.et_welcome_pwd);
+                et_welcome_name = (EditText) pop_View.findViewById(R.id.et_welcome_name);
 
                 WindowManager windowManager = getWindowManager();
                 int width = windowManager.getDefaultDisplay().getWidth();
@@ -113,10 +122,60 @@ public class WelcomeActivity extends Activity {
                 //点击弹出菜单外的其他部分，弹出菜单消失
                 popupWindow.setOnDismissListener(new Popupwindowdismisslistener());
 
+                //获取注册信息，并完成注册
+                final String s_welcome_phoneNum = et_welcome_phoneNum.getText().toString().trim();
+                final String s_welcome_pwd = et_welcome_pwd.getText().toString().trim();
+                final String s_welcome_name = et_welcome_name.getText().toString().trim();
+
+
+                if(s_welcome_phoneNum.length() == 11 && !s_welcome_pwd.equals("") && !s_welcome_name.equals("")) {
+                    btn_welcome_regist.setEnabled(true);
+                    btn_welcome_regist.setTextColor(getResources().getColor(R.color.welcome_regist_color));
+                }
+
+                //注册
+                btn_welcome_finish_regist.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+
+
+                        NLogger.d("ljk","点击完成注册\n" + "s_welcome_phoneNum" + s_welcome_phoneNum + "\ns_welcome_pwd" + s_welcome_pwd
+                        + "s_welcome_name" + s_welcome_name);
+
+
+                        if(s_welcome_phoneNum.length() != 11) {
+
+
+                            Toast.makeText(WelcomeActivity.this,"请输入正确手机号",Toast.LENGTH_SHORT);
+                        }
+
+                        if(s_welcome_pwd.equals("")) {
+                            NLogger.d("ljk","密码不能为空");
+
+                        }
+
+                        if(s_welcome_name.equals("")) {
+                            NLogger.d("ljk","请输入昵称");
+
+                        }
+
+
+                    }
+                });
+
+
+
+
+
 
 
             }
         });
+
+
+
+
 
     }
 
