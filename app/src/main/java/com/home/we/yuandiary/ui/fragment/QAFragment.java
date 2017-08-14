a5db766dda2aff9bae35bc5cce2c7b98de22d97a
yanfeixin123@163.com <yanfeixin123@163.com>
2017-08-14 18:01:27 +0800

    qa 刷新数据

--- app/src/main/java/com/home/we/yuandiary/ui/fragment/QAFragment.java
+++ app/src/main/java/com/home/we/yuandiary/ui/fragment/QAFragment.java
@@ -1,41 +1,27 @@
 package com.home.we.yuandiary.ui.fragment;
 
-import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.ScrollView;
-import android.widget.TextView;
 
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshScrollView;
 import com.home.we.yuandiary.R;
-import com.home.we.yuandiary.litehttp.AppApi;
-import com.home.we.yuandiary.litehttp.AppContext;
-import com.home.we.yuandiary.litehttp.BannerBean;
-import com.home.we.yuandiary.model.Phone;
-import com.home.we.yuandiary.model.QuestionAndAnswer;
 import com.home.we.yuandiary.ui.util.MeiTuanHeaderLayout;
-import com.litesuits.http.LiteHttp;
-import com.litesuits.http.data.GsonImpl;
-import com.litesuits.http.exception.HttpException;
-import com.litesuits.http.impl.huc.HttpUrlClient;
-import com.litesuits.http.listener.HttpListener;
-import com.litesuits.http.request.AbstractRequest;
-import com.litesuits.http.request.BitmapRequest;
-import com.litesuits.http.request.JsonRequest;
-import com.litesuits.http.request.StringRequest;
-import com.litesuits.http.request.param.HttpMethods;
-import com.litesuits.http.response.Response;
-import com.litesuits.http.utils.HttpUtil;
 
-import cn.jesse.nativelogger.NLogger;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 
 /**
@@ -49,10 +35,21 @@ public class QAFragment extends Fragment {
     public static final String url = "http://baidu.com";
     public static final String yuanDiaryUrl = "http://114.215.238.246/api?padapi=questask-asklist.php";
 
-    protected static LiteHttp liteHttp;
-
+    private ListView lv_qa;
+    private ArrayAdapter<String> madapter;
 
     private PullToRefreshScrollView mPullToRefreshScrollView;
+
+    /***********************test**************************/
+
+    private String[] datas = {"apple","Banana","Orange","apple","Banana","Orange"};
+
+    private LinkedList<String> listItems = new LinkedList<>();
+
+
+    /*************************************************/
+
+
 
 
     public static QAFragment newInstance(String info) {
@@ -67,80 +64,77 @@ public class QAFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //initLiteHttp();
-        getDemo();
-
-
-    }
-
-    /**
-     * 单例 keep an singleton instance of litehttp
-     */
-    private void initLiteHttp() {
-
-        AppApi.getBitmap(getContext(), new HttpListener<Bitmap>(false, true, false) {
-            @Override
-            public void onSuccess(Bitmap bitmap, Response<Bitmap> response) {
-                super.onSuccess(bitmap, response);
-                Log.i("DXY", "onSuccess: 成功");
-            }
-
-            @Override
-            public void onFailure(HttpException e, Response<Bitmap> response) {
-                super.onFailure(e, response);
-                Log.i("DXY", "onSuccess: 失败");
-            }
-
-            @Override
-            public void onLoading(AbstractRequest<Bitmap> request, long total, long len) {
-                super.onLoading(request, total, len);
-                Log.i("DXY", "onSuccess: 下载中..." + "total>>>>" + total + "<<<<<len>>>>" + len);
-            }
-        });
-
-
 
 
 
     }
 
 
-    public void getDemo() {
-        // TODO: 缺少RUL
-        AppContext.getHttp(getContext()).executeAsync(new JsonRequest<QuestionAndAnswer>(new AppApi.GetQuestionAndAnswer(), QuestionAndAnswer.class)
-                .setHttpListener(new HttpListener<QuestionAndAnswer>() {
-                    @Override
-                    public void onSuccess(QuestionAndAnswer bannerBean, Response<QuestionAndAnswer> response) {
-                        super.onSuccess(bannerBean, response);
-                        Log.i("DXY", "onSuccess: PostDemo " + bannerBean.getData().get(0).getPicstr());
-                    }
 
-                    @Override
-                    public void onFailure(HttpException e, Response<QuestionAndAnswer> response) {
-                        super.onFailure(e, response);
-                    }
-                }));
 
-    }
+
 
 
     private void initEvents() {
+        //刚开始模拟数据添加
+        listItems.addAll(Arrays.asList(datas));
+        madapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,listItems);
+        lv_qa.setAdapter(madapter);
+
+        //解决ScroolView 嵌套 ListView 只显示一行的问题，ScroolView 无法计算listView 的行高，需要自己设置
+        setListViewHeightBasedOnChildren(lv_qa);
+
+
         mPullToRefreshScrollView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ScrollView>() {
 
             @Override
             public void onRefresh(PullToRefreshBase<ScrollView> refreshView) {
                 new GetDataTask().execute();
+
+                //1.添加数据
+
+
+                //2.新加载数据
             }
+
+
         });
+    }
+
+    private void setListViewHeightBasedOnChildren(ListView listView) {
+        // 获取ListView对应的Adapter
+        ListAdapter listAdapter = listView.getAdapter();
+        if (listAdapter == null) {
+            return;
+        }
+
+        int totalHeight = 0;
+        for (int i = 0, len = listAdapter.getCount(); i < len; i++) {
+            // listAdapter.getCount()返回数据项的数目
+            View listItem = listAdapter.getView(i, null, listView);
+            // 计算子项View 的宽高
+            listItem.measure(0, 0);
+            // 统计所有子项的总高度
+            totalHeight += listItem.getMeasuredHeight();
+        }
+
+        ViewGroup.LayoutParams params = listView.getLayoutParams();
+        params.height = totalHeight+ (listView.getDividerHeight() * (listAdapter.getCount() - 1));
+        // listView.getDividerHeight()获取子项间分隔符占用的高度
+        // params.height最后得到整个ListView完整显示需要的高度
+        listView.setLayoutParams(params);
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_qa, null);
+        lv_qa = (ListView) view.findViewById(R.id.lv_qa);
 
 
-        mPullToRefreshScrollView = (PullToRefreshScrollView) view.findViewById(R.id.main_act_scrollview);
+
+        mPullToRefreshScrollView = (PullToRefreshScrollView) view.findViewById(R.id.qa_scrollview);
         mPullToRefreshScrollView.setHeaderLayout(new MeiTuanHeaderLayout(getActivity()));
         initEvents();
 
@@ -149,23 +143,27 @@ public class QAFragment extends Fragment {
     }
 
 
-    private class GetDataTask extends AsyncTask<Void, Void, String[]> {
+    private class GetDataTask extends AsyncTask<Object, Object, String> {
 
         @Override
-        protected String[] doInBackground(Void... params) {
+        protected String doInBackground(Object... params) {
             // Simulates a background job.
             try {
-                Thread.sleep(3000);
+                Thread.sleep(1500);
             } catch (InterruptedException e) {
             }
-            return null;
+            return "this is new fruit";
         }
 
         @Override
-        protected void onPostExecute(String[] result) {
+        protected void onPostExecute(String result) {
             // Do some stuff here
 
             // Call onRefreshComplete when the list has been refreshed.
+            listItems.addFirst(result);
+            //重新测量listview 的高度
+            setListViewHeightBasedOnChildren(lv_qa);
+            madapter.notifyDataSetChanged();
             mPullToRefreshScrollView.onRefreshComplete();
 
             super.onPostExecute(result);
