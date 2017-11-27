1c08f184fde9ba2aa8ff27eebd4c3b1cfe4e1f93
yanfeixin123@163.com <yanfeixin123@163.com>
2017-11-27 09:46:14 +0800

    登录注册

--- app/src/main/java/com/home/we/yuandiary/ui/module/main/LoginPageActivity.java
+++ app/src/main/java/com/home/we/yuandiary/ui/module/main/LoginPageActivity.java
@@ -1,11 +1,27 @@
 package com.home.we.yuandiary.ui.module.main;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.home.we.yuandiary.R;
+import com.home.we.yuandiary.bean.LoginFailed;
+import com.home.we.yuandiary.bean.LoginFedBack;
+import com.home.we.yuandiary.common.util.GsonUtil;
+import com.home.we.yuandiary.litehttp.AppApi;
+import com.home.we.yuandiary.litehttp.AppContext;
+import com.home.we.yuandiary.ui.fragment.MineFragment;
+import com.litesuits.http.exception.HttpException;
+import com.litesuits.http.listener.HttpListener;
+import com.litesuits.http.response.Response;
+
+import cn.jesse.nativelogger.NLogger;
 
 /**
  * Created by pactera on 2017/11/19.
@@ -13,12 +29,92 @@ import com.home.we.yuandiary.R;
 
 public class LoginPageActivity extends Activity {
 
+    private TextView m_tv_LoginNowRegist;
+    private Button m_btn_login;
+    private EditText m_et_login_username;
+    private EditText m_et_login_pass;
+
 
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_regist);
+        setContentView(R.layout.activity_login);
+
+        initViews();
+
+
+    }
+
+    private void initViews() {
+
+        m_tv_LoginNowRegist = (TextView) findViewById(R.id.tv_login_now_regist);
+        m_et_login_username = (EditText) findViewById(R.id.et_login_username);
+        m_et_login_pass = (EditText) findViewById(R.id.et_login_pass);
+
+
+        m_btn_login = (Button) findViewById(R.id.btn_login_login);
+
+        //点击登录按钮
+        m_btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NLogger.d("ljk","---------------》点击登录按钮");
+
+                //获取注册信息
+                String s_login_phoneNum = m_et_login_username.getText().toString().trim();
+                String s_login_pwd = m_et_login_pass.getText().toString().trim();
+
+                NLogger.d("ljk","用户名" + s_login_phoneNum + "\n密码" + s_login_pwd);
+
+
+                AppContext.getHttp(LoginPageActivity.this)
+                        .executeAsync(new AppApi.LoginParam(s_login_phoneNum,s_login_pwd))
+                        .setHttpListener(new HttpListener<LoginFedBack>() {
+                            @Override
+                            public void onSuccess(LoginFedBack loginFedBack, Response<LoginFedBack> response) {
+                                super.onSuccess(loginFedBack, response);
+                                NLogger.d("ljk","LOGIN onSuccess");
+                                String flag = loginFedBack.getFlag();
+                                NLogger.d("ljk", "登录标识" + response.toString());
+
+                                if(flag.equals("001")) { //登录成功 返回到个人中心页面
+
+                                    Toast.makeText(LoginPageActivity.this, "登录成功！",Toast.LENGTH_SHORT).show();
+
+
+                                }else {
+
+                                }
+
+                            }
+
+                            @Override
+                            public void onFailure(HttpException e, Response<LoginFedBack> response) {
+                                super.onFailure(e, response);
+
+
+                            }
+                        });
+
+
+            }
+        });
+
+
+        //点击立即注册按钮，跳转到注册页面
+        m_tv_LoginNowRegist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                skipRegistPage();
+            }
+
+            private void skipRegistPage() {
+                Intent intent = new Intent(LoginPageActivity.this,RegistPageActivity.class);
+                startActivity(intent);
+            }
+
+        });
 
     }
 }
--- app/src/main/java/com/home/we/yuandiary/ui/module/main/RegistPageActivity.java
+++ app/src/main/java/com/home/we/yuandiary/ui/module/main/RegistPageActivity.java
@@ -1,10 +1,27 @@
 package com.home.we.yuandiary.ui.module.main;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.home.we.yuandiary.R;
+import com.home.we.yuandiary.bean.RegistData;
+import com.home.we.yuandiary.litehttp.AppApi;
+import com.litesuits.http.exception.HttpException;
+import com.litesuits.http.listener.HttpListener;
+import com.litesuits.http.response.Response;
+
+import java.util.HashMap;
+
+import cn.jesse.nativelogger.NLogger;
+
+import static com.home.we.yuandiary.R.id.et_welcome_pwd;
 
 /**
  * Created by pactera on 2017/11/19.
@@ -12,9 +29,96 @@ import com.home.we.yuandiary.R;
 
 public class RegistPageActivity extends Activity {
 
+    private Button m_Btn_Regist;
+    private EditText m_et_regist_phoneNum;
+    private EditText m_et_regist_pass;
+    private EditText m_et_regist_sure_pass;
+
+    private TextView m_tv_already_regist;
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_regist);
+
+        initViews();
+
+        m_Btn_Regist = (Button) findViewById(R.id.btn_regist_regist);
+        m_et_regist_phoneNum = (EditText) findViewById(R.id.et_regist_phoneNum);
+        m_et_regist_pass = (EditText) findViewById(R.id.et_regist_pass);
+        m_et_regist_sure_pass = (EditText) findViewById(R.id.et_regist_sure_pass);
+
+        m_tv_already_regist = (TextView) findViewById(R.id.tv_already_regist);
+
+        //已经注册，返回登录按钮
+        m_tv_already_regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                skipToLoginPage();
+            }
+
+            private void skipToLoginPage() {
+
+                Intent intent = new Intent(RegistPageActivity.this,LoginPageActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //点击注册按钮处理注册逻辑
+        m_Btn_Regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                regist();
+
+            }
+
+            private void regist() {
+
+                String s_et_regist_phoneNum = m_et_regist_phoneNum.getText().toString().trim();
+                String s_et_regist_pass = m_et_regist_pass.getText().toString().trim();
+                String s_et_regist_sure_pass = m_et_regist_sure_pass.getText().toString().trim();
+
+
+
+
+                HashMap<String, String> postDatas = new HashMap<String, String>();
+                //postDatas.put("alias","开飞机的舒克");
+                postDatas.put("username",s_et_regist_phoneNum);
+                postDatas.put("password",s_et_regist_pass);
+                postDatas.put("topassword",s_et_regist_sure_pass);
+
+
+
+
+
+                AppApi.postForRegist(RegistPageActivity.this, null, new HttpListener<RegistData>() {
+                    @Override
+                    public void onSuccess(RegistData registData, Response<RegistData> response) {
+                        super.onSuccess(registData, response);
+                        String fedBack_regist_message = registData.getData();
+                        //NLogger.d("ljk","注册返回数据\n" + registData.toString());
+                        NLogger.d("ljk", response.toString());
+                        Toast.makeText(RegistPageActivity.this, fedBack_regist_message,Toast.LENGTH_SHORT).show();
+
+
+                    }
+
+                    @Override
+                    public void onFailure(HttpException e, Response<RegistData> response) {
+                        super.onFailure(e, response);
+                    }
+
+
+
+                },postDatas);
+            }
+        });
+    }
+
+    private void initViews() {
+
     }
 }
